e660b872f9c7d00e8a4eb41186ce8d1e88377cdd
Kantinanjintana <137372197+Kantinanjintana@users.noreply.github.com>
2026-01-22 15:06:06 +0700

    pokemon

--- app/index.tsx
+++ app/index.tsx
@@ -97,6 +97,14 @@ export default function Index() {
         <Link href="/location" style={styles.link}>
           <Text style={styles.linkText}>Location</Text>
         </Link>
+
+        <Link href="/ant" style={styles.link}>
+          <Text style={styles.linkText}>spd</Text>
+        </Link>
+
+        <Link href="/Charmander" style={styles.link}>
+          <Text style={styles.linkText}>kakarot</Text>
+        </Link>
       </View>
     </ScrollView>
   );
